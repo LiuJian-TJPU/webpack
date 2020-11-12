77590cd487b831814df70a1c3c043cec892bce68
刘健 <17602656463@163.com>
2020-11-12 10:34:38 +0800

    install

---
+++ install.sh
@@ -0,0 +1,3 @@
+yarn add @babel/core @babel/plugin-proposal-class-properties @babel/plugin-proposal-decorators @babel/preset-env @babel/preset-react autoprefixer babel-loader babel-plugin-import clean-webpack-plugin cross-env css-loader file-loader html-webpack-plugin less less-loader mini-css-extract-plugin optimize-css-assets-webpack-plugin postcss-loader style-loader uglifyjs-webpack-plugin url-loader webpack webpack-cli webpack-dev-server webpack-merge -D
+
+yarn add react react-dom react-redux redux antd
